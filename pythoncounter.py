fb7a0fa6aa193a8009767a67f1d54cbc8bfd162f
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 15:38:12 +0200

    Skrivit om med en for loop

--- pythoncounter.py
+++ pythoncounter.py
@@ -11,6 +11,9 @@ class Product:
 
 products = [Product(price = 900, count = 2, tax = 1.25), Product(price = 100, count = 1, tax = 1.06), Product(price = 1000, count = 1, tax = 1.25)]
 
-total_price = products[0].price_with_tax() + products[1].price_with_tax() + products[2].price_with_tax()
+total_price = 0
 
-print (total_price)
+for product in products:
+	total_price += product.price_with_tax()
+
+print(total_price)
