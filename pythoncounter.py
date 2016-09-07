b8c42b1fbfe561f2fb760f38443f7660de75e272
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 14:40:54 +0200

    Skrev om med klass med konstruktor

--- pythoncounter.py
+++ pythoncounter.py
@@ -1,18 +1,19 @@
 class Product:
 	price = 0
+	service = 0
 	count = 0
 	tax = 0
+	def __init__ (self, price, service, count, tax):
+		self.price = price
+		self.service = service
+		self.count = count
+		self.tax = tax
 	def price_with_tax(self):
-		return self.price * self.count * self.tax	
+		return self.price * self.service * self.count * self.tax
+		
 
-robot = Product()
-robot.price = 900
-robot.count = 2
-robot.tax = 1.25
+robot = Product(price = 900, service = 10, count = 2, tax = 1.25)	
+book = Product(price = 100, service = 10, count = 1, tax = 1.06)
+husvagn = Product(price = 1000, service = 10, count = 1, tax = 1.25)
 
-book = Product()
-book.price = 100
-book.count = 1
-book.tax = 1.06
-
-print(robot.price_with_tax() + book.price_with_tax())
+print(robot.price_with_tax() + book.price_with_tax() + husvagn.price_with_tax())
