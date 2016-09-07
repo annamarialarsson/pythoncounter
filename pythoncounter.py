d5df9d3b04a96421914dba72d61f332fbf4e3991
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 15:01:20 +0200

    Skrev om som en lista av produkter

--- pythoncounter.py
+++ pythoncounter.py
@@ -1,19 +1,16 @@
 class Product:
 	price = 0
-	service = 0
 	count = 0
 	tax = 0
-	def __init__ (self, price, service, count, tax):
+	def __init__ (self, price, count, tax):
 		self.price = price
-		self.service = service
 		self.count = count
 		self.tax = tax
 	def price_with_tax(self):
-		return self.price * self.service * self.count * self.tax
-		
+		return self.price * self.count * self.tax
 
-robot = Product(price = 900, service = 10, count = 2, tax = 1.25)	
-book = Product(price = 100, service = 10, count = 1, tax = 1.06)
-husvagn = Product(price = 1000, service = 10, count = 1, tax = 1.25)
+products = [Product(price = 900, count = 2, tax = 1.25), Product(price = 100, count = 1, tax = 1.06), Product(price = 1000, count = 1, tax = 1.25)]
 
-print(robot.price_with_tax() + book.price_with_tax() + husvagn.price_with_tax())
+total_price = products[0].price_with_tax() + products[1].price_with_tax() + products[2].price_with_tax()
+
+print (total_price)
