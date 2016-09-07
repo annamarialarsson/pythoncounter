7641e25e7927ae7cbb6debdbd5b263fabf90a96d
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 16:04:38 +0200

    Lagt till rabatt

--- pythoncounter.py
+++ pythoncounter.py
@@ -7,7 +7,11 @@ class Product:
 		self.count = count
 		self.tax = tax
 	def price_with_tax(self):
-		return self.price * self.count * self.tax
+		itemtotal = self.price * self.count * self.tax
+		if itemtotal > 500:
+			return 0.9 * itemtotal
+		else:
+			return itemtotal	
 
 products = [Product(price = 900, count = 2, tax = 1.25), Product(price = 100, count = 1, tax = 1.06), Product(price = 1000, count = 1, tax = 1.25)]
 
