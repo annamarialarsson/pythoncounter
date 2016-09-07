22a97d206a800f49d05b420e5c60d21fe4b55c5e
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 12:12:02 +0200

    Skrev om genom att använda klasser

---
+++ pythoncounter.py
@@ -0,0 +1,19 @@
+class Product:
+	price = 0
+	count = 0
+	tax = 0
+
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
+
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
+
+
+print(robot.price * robot.count * robot.tax + book.price * book.count * book.tax)
+
+
