e12286301bbfe335d7b2db6415faa50abbe5d655
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 16:49:38 +0200

    Driver ut namn och pris på produkterna

--- pythoncounter.py
+++ pythoncounter.py
@@ -2,10 +2,12 @@ class Product:
 	price = 0
 	count = 0
 	tax = 0
-	def __init__ (self, price, count, tax):
+	name = "0"
+	def __init__ (self, price, count, tax, name):
 		self.price = price
 		self.count = count
 		self.tax = tax
+		self.name = name
 	def price_with_tax(self):
 		itemtotal = self.price * self.count * self.tax
 		if itemtotal > 500:
@@ -13,11 +15,14 @@ class Product:
 		else:
 			return itemtotal	
 
-products = [Product(price = 900, count = 2, tax = 1.25), Product(price = 100, count = 1, tax = 1.06), Product(price = 1000, count = 1, tax = 1.25)]
+products = [Product(price = 900, count = 2, tax = 1.25, name = "Mattsson"), Product(price = 100, count = 1, tax = 1.06, name = "Instruktionsbok"), Product(price = 1000, count = 1, tax = 1.25, name = "Husvagn"), Product(price = 100, count = 1, tax = 1.25, name = "Luftballong")]
 
 total_price = 0
 
 for product in products:
 	total_price += product.price_with_tax()
+
+for product in products:
+	print(product.name, product.price_with_tax())
 
 print(total_price)
