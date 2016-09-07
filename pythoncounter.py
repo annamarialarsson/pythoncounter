2f75017ed863f5f4bb4fb5152084bc5573da0bae
annamarialarsson <annamaria.larsson@aventyret.com>
2016-09-07 13:29:31 +0200

    Skrev om genom att använda klass och metod

--- pythoncounter.py
+++ pythoncounter.py
@@ -2,6 +2,8 @@ class Product:
 	price = 0
 	count = 0
 	tax = 0
+	def price_with_tax(self):
+		return self.price * self.count * self.tax	
 
 robot = Product()
 robot.price = 900
@@ -13,7 +15,4 @@ book.price = 100
 book.count = 1
 book.tax = 1.06
 
-
-print(robot.price * robot.count * robot.tax + book.price * book.count * book.tax)
-
-
+print(robot.price_with_tax() + book.price_with_tax())
